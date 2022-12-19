c727b0661fd64cdc9a5f535099715a64add49c99
OfficialB <reconispog@gmail.com>
2022-12-18 22:45:46 -0800

    var.py comments

--- src/conf/var.py
+++ src/conf/var.py
@@ -1,4 +1,4 @@
 # Config variables
-creator = "Official B"
-product = "Social Sleuth"
-v = "2.0.0.beta-3"
+creator = "Official B" # Who made the product / fork?
+product = "Social Sleuth" # What is the product?
+v = "2.0.0.beta-3" # What is the product version?
