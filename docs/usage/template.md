e4c99b21df9954f2906fdc7d4264e135dd7f90e0
OfficialB <reconispog@gmail.com>
2022-12-18 16:52:14 -0800

    Clean code up, move links into a list & into a different file, template changed

--- docs/usage/template.md
+++ docs/usage/template.md
@@ -1,5 +1,5 @@
 # Link Template
 Here is the standard template for links that you can use in src\sleuth.py.
 ```python
-f"\nhttps://LINK.com/{user}"
+"\nhttps://{user}.example.com",
 ```
--- src/sleuth.py
+++ src/sleuth.py
@@ -1,6 +1,7 @@
 # Import necessary modules (run sh/install_requirements.sh if you get any error regarding these. If that doesn't fix it create an issue on the repo.)
 import argparse
 import conf.var as var
+from conf.links import links_list
 
 # Create an ArgumentParser object
 parser = argparse.ArgumentParser()
@@ -17,49 +18,9 @@ user = args.user
 # Prints out links if the user argument has a value
 if user:
         # Basic program information
-        links = f"\nYou are currently running {var.product} v{var.v}, created by {var.creator}"
-        links += "\n----------------------------------------------" 
+        links_string = f"\nYou are currently running {var.product} v{var.v}, created by {var.creator}"
+        links_string += "\n----------------------------------------------" 
         # Links
-        links += f"\nhttps://twitter.com/{user}"
-        links += f"\nhttps://www.facebook.com/{user}"
-        links += f"\nhttps://linkedin.com/in/{user}"
-        links += f"\nhttps://www.snapchat.com/app/{user}"
-        links += f"\nhttps://www.instagram.com/{user}"
-        links += f"\nhttps://www.tiktok.com/@{user}"
-        links += f"\nhttps://www.pinterest.com/{user}"
-        links += f"\nhttps://www.tumblr.com/blog/view/{user}"
-        links += f"\nhttps://www.reddit.com/user/{user}"
-        links += f"\nhttps://github.com/{user}"
-        links += f"\nhttps://www.flickr.com/photos/{user}"
-        links += f"\nhttps://soundcloud.com/{user}"
-        links += f"\nhttps://t.me/{user}"
-        links += f"\nhttps://www.bitview.net/profile.php?user={user}"
-        links += f"\nhttps://www.vidlii.com/user/{user}"
-        links += f"\nhttps://www.guilded.gg/{user}"                  
-        links += f"\nhttps://www.guilded.gg/u/{user}"
-        links += f"\nhttps://bwitter.me/{user}"
-        links += f"\nhttps://youtube.com/user/{user}"
-        links += f"\nhttps://codepen.io/{user}"
-        links += f"\nhttps://pastebin.com/u/{user}"
-        links += f"\nhttps://replit.com/@{user}"
-        links += f"\nhttps://{user}.carrd.co"
-        links += f"\nhttps://www.crunchyroll.comc/user/{user}"
-        links += f"\nhttps://scratch.mit.edu/users/{user}"
-        links += f"\nhttps://discords.com/bio/p/{user}"
-        links += f"\nhttps://www.twitch.tv/{user}"
-        links += f"\nhttps://{user}.newgrounds.com"
-        links += f"\nhttps://archive.org/details/@{user}"
-        links += f"\nhttps://{user}.github.io"
-        links += f"\nhttps://vimeo.com/{user}"
-        links += f"\nhttps://youtube.com/@{user}"
-        links += f"\nhttps://about.me/{user}"
-        links += f"\nhttps://{user}.ct8.pl"
-        links += f"\nhttps://community.brave.com/u/{user}"
-        links += f"\nhttps://{user}.infinityfreeapp.com"
-        links += f"\nhttps://{user}.rf.gd"
-        links += f"\nhttps://{user}.great-site.net"
-        links += f"\nhttps://{user}.epizy.com"
-        links += f"\nhttps://{user}.42web.io"
-        links += f"\nhttps://{user}.lovestoblog.com"
-
-        print(links)
+        links_string += "\n".join(link.format(user=user) for link in links_list)
+        
+        print(links_string)
