04d0d5cb33afa8858ea5b82dd43c1f23f4ab64b6
OfficialB <reconispog@gmail.com>
2022-12-18 16:19:39 -0800

    Fix "none" printing at the end of the links bug
    Took a bit to fix, but I got it.

--- src/sleuth.py
+++ src/sleuth.py
@@ -6,7 +6,7 @@ import conf.var as var
 parser = argparse.ArgumentParser()
 
 # Add an argument with a short and long option
-parser.add_argument("-u", "--username", required=True, help="Username to look up")
+parser.add_argument("-u", "--user", required=True, help="Username to look up")
 
 # Parse the command line arguments
 args = parser.parse_args()
@@ -16,50 +16,50 @@ user = args.user
 
 # Prints out links if the user argument has a value
 if user:
-    print(
-            # Basic program information
-            f"\nYou are currently running {var.product} v{var.v}, created by {var.creator}"
-            "\n----------------------------------------------" + 
-            # Links
-            f"\nhttps://twitter.com/{user}" +
-            f"\nhttps://www.facebook.com/{user}" +
-            f"\nhttps://linkedin.com/in/{user}" +
-            f"\nhttps://www.snapchat.com/app/{user}" +
-            f"\nhttps://www.instagram.com/{user}" +
-            f"\nhttps://www.tiktok.com/@{user}" +
-            f"\nhttps://www.pinterest.com/{user}" +
-            f"\nhttps://www.tumblr.com/blog/view/{user}" +
-            f"\nhttps://www.reddit.com/user/{user}" +
-            f"\nhttps://github.com/{user}" +
-            f"\nhttps://www.flickr.com/photos/{user}" +
-            f"\nhttps://soundcloud.com/{user}" +
-            f"\nhttps://t.me/{user}" +
-            f"\nhttps://www.bitview.net/profile.php?user={user}" +
-            f"\nhttps://www.vidlii.com/user/{user}" +
-            f"\nhttps://www.guilded.gg/{user}" +                     
-            f"\nhttps://www.guilded.gg/u/{user}" +
-            f"\nhttps://bwitter.me/{user}" +   
-            f"\nhttps://youtube.com/user/{user}" +   
-            f"\nhttps://codepen.io/{user}" +   
-            f"\nhttps://pastebin.com/u/{user}" +   
-            f"\nhttps://replit.com/@{user}" +   
-            f"\nhttps://{user}.carrd.co" +   
-            f"\nhttps://www.crunchyroll.comc/user/{user}" +   
-            f"\nhttps://scratch.mit.edu/users/{user}" +   
-            f"\nhttps://discords.com/bio/p/{user}" +   
-            f"\nhttps://www.twitch.tv/{user}" +   
-            f"\nhttps://{user}.newgrounds.com" +   
-            f"\nhttps://archive.org/details/@{user}" +   
-            f"\nhttps://{user}.github.io" +   
-            f"\nhttps://vimeo.com/{user}" +   
-            f"\nhttps://youtube.com/@{user}" +   
-            f"\nhttps://about.me/{user}" +   
-            f"\nhttps://{user}.ct8.pl" +   
-            f"\nhttps://community.brave.com/u/{user}" +   
-            f"\nhttps://{user}.infinityfreeapp.com" +   
-            f"\nhttps://{user}.rf.gd" +   
-            f"\nhttps://{user}.great-site.net" +   
-            f"\nhttps://{user}.epizy.com" +   
-            f"\nhttps:///{user}.42web.io" +   
-            f"\nhttps:///{user}.lovestoblog.com"   
-        )
+        # Basic program information
+        links = f"\nYou are currently running {var.product} v{var.v}, created by {var.creator}"
+        links += "\n----------------------------------------------" 
+        # Links
+        links += f"\nhttps://twitter.com/{user}"
+        links += f"\nhttps://www.facebook.com/{user}"
+        links += f"\nhttps://linkedin.com/in/{user}"
+        links += f"\nhttps://www.snapchat.com/app/{user}"
+        links += f"\nhttps://www.instagram.com/{user}"
+        links += f"\nhttps://www.tiktok.com/@{user}"
+        links += f"\nhttps://www.pinterest.com/{user}"
+        links += f"\nhttps://www.tumblr.com/blog/view/{user}"
+        links += f"\nhttps://www.reddit.com/user/{user}"
+        links += f"\nhttps://github.com/{user}"
+        links += f"\nhttps://www.flickr.com/photos/{user}"
+        links += f"\nhttps://soundcloud.com/{user}"
+        links += f"\nhttps://t.me/{user}"
+        links += f"\nhttps://www.bitview.net/profile.php?user={user}"
+        links += f"\nhttps://www.vidlii.com/user/{user}"
+        links += f"\nhttps://www.guilded.gg/{user}"                  
+        links += f"\nhttps://www.guilded.gg/u/{user}"
+        links += f"\nhttps://bwitter.me/{user}"
+        links += f"\nhttps://youtube.com/user/{user}"
+        links += f"\nhttps://codepen.io/{user}"
+        links += f"\nhttps://pastebin.com/u/{user}"
+        links += f"\nhttps://replit.com/@{user}"
+        links += f"\nhttps://{user}.carrd.co"
+        links += f"\nhttps://www.crunchyroll.comc/user/{user}"
+        links += f"\nhttps://scratch.mit.edu/users/{user}"
+        links += f"\nhttps://discords.com/bio/p/{user}"
+        links += f"\nhttps://www.twitch.tv/{user}"
+        links += f"\nhttps://{user}.newgrounds.com"
+        links += f"\nhttps://archive.org/details/@{user}"
+        links += f"\nhttps://{user}.github.io"
+        links += f"\nhttps://vimeo.com/{user}"
+        links += f"\nhttps://youtube.com/@{user}"
+        links += f"\nhttps://about.me/{user}"
+        links += f"\nhttps://{user}.ct8.pl"
+        links += f"\nhttps://community.brave.com/u/{user}"
+        links += f"\nhttps://{user}.infinityfreeapp.com"
+        links += f"\nhttps://{user}.rf.gd"
+        links += f"\nhttps://{user}.great-site.net"
+        links += f"\nhttps://{user}.epizy.com"
+        links += f"\nhttps://{user}.42web.io"
+        links += f"\nhttps://{user}.lovestoblog.com"
+
+        print(links)
