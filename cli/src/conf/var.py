f43ebc9f972810e465825af36df90bbafcce4ba2
r1c0n <reconispog@gmail.com>
2023-02-02 14:22:36 -0800

    Add the license comment

--- cli/src/conf/var.py
+++ cli/src/conf/var.py
@@ -1,3 +1,22 @@
+#########################################################################
+# Find social media accounts, in bulk, with ease!                       #
+# Copyright (C) 2021 - 2022  r1c0n                                      #
+#                                                                       #
+# This program is free software: you can redistribute it and/or modify  #
+# it under the terms of the GNU General Public License as published by  #
+# the Free Software Foundation, either version 3 of the License, or     #
+# (at your option) any later version.                                   #
+#                                                                       #
+# This program is distributed in the hope that it will be useful,       #
+# but WITHOUT ANY WARRANTY; without even the implied warranty of        #
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         #
+# GNU General Public License for more details.                          #
+#                                                                       #
+# You should have received a copy of the GNU General Public License     #
+# along with this program.  If not, see <https://www.gnu.org/licenses/> #
+#########################################################################
+
+
 # Config variables
 creator = "Official B" # Who made the product / fork?
 product = "Social Sleuth" # What is the product?
--- cli/src/sleuth.py
+++ cli/src/sleuth.py
@@ -1,3 +1,22 @@
+#########################################################################
+# Find social media accounts, in bulk, with ease!                       #
+# Copyright (C) 2021 - 2022  r1c0n                                      #
+#                                                                       #
+# This program is free software: you can redistribute it and/or modify  #
+# it under the terms of the GNU General Public License as published by  #
+# the Free Software Foundation, either version 3 of the License, or     #
+# (at your option) any later version.                                   #
+#                                                                       #
+# This program is distributed in the hope that it will be useful,       #
+# but WITHOUT ANY WARRANTY; without even the implied warranty of        #
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         #
+# GNU General Public License for more details.                          #
+#                                                                       #
+# You should have received a copy of the GNU General Public License     #
+# along with this program.  If not, see <https://www.gnu.org/licenses/> #
+#########################################################################
+
+
 # Import necessary modules
 import argparse
 import conf.var as var
@@ -6,7 +25,7 @@ from conf.links import links_list
 # Create an ArgumentParser object
 parser = argparse.ArgumentParser()
 
-# Add an argument with a short and long option
+# Add the user argument
 parser.add_argument("-u", "--user", required=True, help="Username to look up")
 
 # Parse the command line arguments
